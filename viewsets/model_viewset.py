a7d64ae8183bb4e5d8eb3abe316c219accf4262f
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-01-25 16:13:15 +0100

    Adopts an easier overridable structure.

--- viewsets/model_viewset.py
+++ viewsets/model_viewset.py
@@ -10,8 +10,37 @@ from django.core.urlresolvers import reverse
 
 class ModelViewSet(object):
     model = None
-    views = (b'list_view', b'detail_view', b'create_view', b'update_view',
-             b'delete_view')
+    views = {
+        b'list_view': {
+            b'view': ListView,
+            b'pattern': br'',
+            b'name': b'index',
+        },
+        b'detail_view': {
+            b'view': DetailView,
+            b'pattern': br'(?P<pk>\d+)',  # Change to ’id’?
+            b'name': b'detail',
+        },
+        b'create_view': {
+            b'view': CreateView,
+            b'pattern': br'create',  # Change to ’id’?
+            b'name': b'create',
+        },
+        b'update_view': {
+            b'view': UpdateView,
+            b'pattern': br'(?P<pk>\d+)/update',  # Change to ’id’?
+            b'name': b'update',
+        },
+        b'delete_view': {
+            b'view': DeleteView,
+            b'pattern': br'(?P<pk>\d+)/delete',  # Change to ’id’?
+            b'name': b'delete',
+            b'kwargs': {
+                b'get_success_url': lambda self:
+                                        lambda _: reverse(self.main_url),
+            },
+        },
+    }
     base_url = None
     main_view = b'list_view'
     main_url = None
@@ -22,56 +51,33 @@ class ModelViewSet(object):
         self.model_slug = slugify(self.model._meta.verbose_name)
         app_label = self.model._meta.app_label
         if self.main_url is None:
+            main_view_name = self.views.get(self.main_view).get(b'name')
             self.main_url = b'%s:%s_%s' % (app_label,
                                            self.model_slug,
-                                           getattr(self, self.main_view).name)
+                                           main_view_name)
 
         self.urls = (self.__build_url_patterns(),
                      app_label, app_label)
 
-    def __get_url(self, view_method):
+    def __build_model_generic_view(self, view_dict):
+        view = view_dict[b'view']
+        view.model = self.model
+        for k, v in view_dict.get(b'kwargs', {}).items():
+            if callable(v):
+                v = v(self)
+            setattr(view, k, v)
+        return view.as_view()
+
+    def __get_url(self, view_dict):
         d = {
             b'regex': br'^%s$' % br'/'.join((self.base_url,
-                                             view_method.pattern)),
-            b'view': view_method(),
-            b'name': b'%s_%s' % (self.model_slug, view_method.name),
+                                             view_dict[b'pattern'])),
+            b'view': self.__build_model_generic_view(view_dict),
+            b'name': b'%s_%s' % (self.model_slug, view_dict[b'name']),
         }
         return url(**d)
 
     def __build_url_patterns(self):
         return patterns(b'',
-            *[self.__get_url(getattr(self, view)) for view in self.views]
+            *[self.__get_url(view_dict) for view_dict in self.views.values()]
         )
-
-    def __build_model_generic_view(self, view, **kwargs):
-        view.model = self.model
-        for k, v in kwargs.items():
-            setattr(view, k, v)
-        return view.as_view()
-
-    def list_view(self):
-        return self.__build_model_generic_view(ListView)
-    list_view.pattern = br''
-    list_view.name = b'index'
-
-    def detail_view(self):
-        return self.__build_model_generic_view(DetailView)
-    detail_view.pattern = br'(?P<pk>\d+)'  # Change to ’id’ ?
-    detail_view.name = b'detail'
-
-    def create_view(self):
-        return self.__build_model_generic_view(CreateView)
-    create_view.pattern = br'create'
-    create_view.name = b'create'
-
-    def update_view(self):
-        return self.__build_model_generic_view(UpdateView)
-    update_view.pattern = detail_view.pattern + br'/update'
-    update_view.name = b'update'
-
-    def delete_view(self):
-        get_success_url = lambda _: reverse(self.main_url)
-        return self.__build_model_generic_view(DeleteView,
-                                               get_success_url=get_success_url)
-    delete_view.pattern = detail_view.pattern + br'/delete'
-    delete_view.name = b'delete'
