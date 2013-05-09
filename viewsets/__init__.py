026cdd8400c7bf67bb3eecb5605e9dd20cf5f47e
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-05-09 13:03:45 +0200

    Bumps version number.

--- viewsets/__init__.py
+++ viewsets/__init__.py
@@ -15,7 +15,7 @@ from .patterns import PK, SLUG
 __author__ = 'Bertrand Bordage'
 __credits__ = ('Bertrand Bordage',)
 __license__ = 'BSD License'
-__version__ = '0.1.6'
+__version__ = '0.1.6.post'
 __maintainer__ = 'Bertrand Bordage'
 __email__ = 'bordage.bertrand@gmail.com'
 __status__ = '3 - Alpha'
